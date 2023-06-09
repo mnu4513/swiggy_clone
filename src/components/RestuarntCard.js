90ec58b980cd09b7b72864e2ef1e42478fd25db2
mnu4513 <mnu4513@gmail.com>
2023-06-09 22:44:22 +0530

    .

--- src/components/RestuarntCard.js
+++ src/components/RestuarntCard.js
@@ -1,4 +1,18 @@
+import { imageApi } from "./Constants";
 
-const RestuarantCard = () => {
-    const {cloudinaryImageId, name, avgRating, area, costForTwo, cuisines } = props;
+const RestuarantCard = (props) => {
+    const {cloudinaryImageId, name, avgRating, area, costForTwo, cuisines } = props.data;
+
+    return (
+        <div className="card">
+            <img src={imageApi + cloudinaryImageId} alt="Restuarant Poster"/>
+            <h3> {name} </h3>
+            <p> {cuisines?.join(', ')?.slice(0, 30)} </p>
+            <p> {avgRating} </p>
+            <p> {area} </p>
+            <p> Cost for Two: {costForTwo / 100} </p>
+        </div>
+    );
 };
+
+export default RestuarantCard;
