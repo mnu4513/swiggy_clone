5a709adeea0b4670cbf2e2e42668625941a6c71a
mnu4513 <mnu4513@gmail.com>
2023-06-09 22:42:59 +0530

    .

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {swiggyApi} from "./Constants";
+import RestuarantCard from "./RestuarntCard";
 
 const Body = () => {
 
@@ -13,14 +14,20 @@ const Body = () => {
         const josnData = await fetch(swiggyApi);
         const data = await josnData.json();
         console.log(data);
+        console.log(data.data.cards[2].data.data.cards);
+        setList(data.data.cards[2].data.data.cards);
     };
 
 
     return (
-        <>
-
-        </>
-    )
+        <div className="restuarant-list">
+            {
+                list?.map((item) => {
+                    return <RestuarantCard data={item.data} key={item.data.id}/>
+                })
+            }
+        </div>
+    );
 };
 
 export default Body;
