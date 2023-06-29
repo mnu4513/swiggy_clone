42c8d90803fd1c9e889cddd4456980bd6422628a
mnu4513 <mnu4513@gmail.com>
2023-06-29 14:10:29 +0530

    .

--- src/components/Constants.js
+++
@@ -1,7 +0,0 @@
-const swiggyApi = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.9124336&lng=75.7872709&page_type=DESKTOP_WEB_LISTING";
-
-const imageApi = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/"
-
-const restuarantList = [];
-
-module.exports = {swiggyApi, imageApi, restuarantList}
